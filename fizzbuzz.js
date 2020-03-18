67fb5d0b146ae1d5746e51b81df98cd59f5f0cad
nevelson <nevelson92@gmail.com>
2020-03-18 15:41:13 -0400

    Fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,18 +2,46 @@
 
 $('#number-chooser').submit(event => {
   event.preventDefault();
+  $('.js-results').empty();
   const number = $('#number-choice').val();
-  console.log(number);
   $('#number-choice').val('');
-  $('.js-results').append(
-    `<div class="fizz-buzz-item">
-      <span>${number
-      }</span>
-    </div>`
-  );
+
+  for (let i = 1; i <= number; i++) {
+    let returnValue = fizzbuzzReplace(i);
+    $('.js-results').append(
+      `<div class="fizz-buzz-item">
+        <span>${returnValue}</span>
+      </div>`);
+    fizzbuzzClassAdd(returnValue);
+  }
 });
 
 
+function fizzbuzzReplace(currentNum) {
+  let word = '';
+  if (currentNum % 5 === 0 && currentNum % 3 === 0) {
+    word = 'fizzbuzz';
+    return word;
+  }
+  if (currentNum % 3 === 0) {
+    word = 'fizz';
+    return word;
+  }
+  if (currentNum % 5 === 0) {
+    word = 'buzz';
+    return word;
+  }
+  return currentNum;
+}
 
-
-
+function fizzbuzzClassAdd(returnValue) {
+  if (returnValue === 'fizzbuzz') {
+    $('.js-results .fizz-buzz-item').last().addClass('fizzbuzz');
+  }
+  if (returnValue === 'fizz') {
+    $('.js-results .fizz-buzz-item').last().addClass('fizz');
+  }
+  if (returnValue === 'buzz') {
+    $('.js-results .fizz-buzz-item').last().addClass('buzz');
+  }
+}
